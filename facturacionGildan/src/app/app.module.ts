98831ded603a0dbf22b251db5470bd12847d0c70
Lesly Peña <pleslysarahi@gmail.com>
2021-07-11 12:36:41 -0600

    add menu, login e inicio

--- facturacionGildan/src/app/app.module.ts
+++ facturacionGildan/src/app/app.module.ts
@@ -3,16 +3,111 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+
+// Importaciones de componentes ng-zorro
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzCalendarModule } from 'ng-zorro-antd/calendar';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+///////////////////////////////////
+import { MenuComponent } from './pages/menu/menu.component';
+import { LoginComponent } from './login/login.component';
+
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IconsProviderModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    /////////////////////////
+    // NgZorroAntdModule,
+    NzTabsModule,
+    NzBadgeModule,
+    NzCardModule,
+    NzGridModule,
+    NzLayoutModule,
+    NzTableModule,
+    NzMenuModule,
+    NzDropDownModule,
+    NzButtonModule,
+    NzIconModule,
+    NzModalModule,
+    NzDrawerModule,
+    NzRadioModule,
+    NzFormModule,
+    NzPaginationModule,
+    NzSelectModule,
+    NzInputNumberModule,
+    NzInputModule,
+    NzAlertModule,
+    NzSwitchModule,
+    NzMessageModule,
+    NzNotificationModule,
+    NzPaginationModule,
+    NzCheckboxModule,
+    NzDatePickerModule,
+    NzTimePickerModule,
+    NzEmptyModule,
+    NzListModule,
+    NzStatisticModule,
+    NzDividerModule,
+    NzSpinModule,
+    NzCalendarModule,
+    NzToolTipModule,
+    NzSkeletonModule,
+    ScrollingModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
